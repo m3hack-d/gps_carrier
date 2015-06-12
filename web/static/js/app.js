f971094efad4a0c442238ca545504eb9f97d2780
Takayuki Matsubara <takayuki.1229@gmail.com>
2015-06-12 11:21:48 +0900

    add video tag

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -11,6 +11,22 @@ if (navigator.geolocation) {
   alert("not supported");
 }
 
+
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || window.navigator.mozGetUserMedia;
+window.URL = window.URL || window.webkitURL;
+
+var video = document.getElementById('myVideo');
+var localStream = null;
+navigator.getUserMedia({video: true, audio: false},
+  function(stream) { // for success case
+    console.log(stream);
+    video.src = window.URL.createObjectURL(stream);
+  },
+  function(err) { // for error case
+    console.log(err);
+  }
+);
+
 function success(position) {
   console.log(position.coords.latitude);
   console.log(position.coords.longitude);
